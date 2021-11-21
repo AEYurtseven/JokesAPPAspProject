68189505fa550f89f28d07eaa7b695d3cc0c9862
Ahmet Erdal Yurtseven <32684419+AEYurtseven@users.noreply.github.com>
2021-11-21 21:30:55 +0000

    Add files via upload

---
+++ models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public int JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+    }
+}
